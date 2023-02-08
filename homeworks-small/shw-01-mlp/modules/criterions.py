54d9e1f93b84c4c262b85f4ffe93f8e81d28cab8
Dmitry Zharikov <dimahzharikov10@gmail.com>
2023-02-08 18:58:08 +0300

    [ADD] First small HW

--- homeworks-small/shw-01-mlp/modules/criterions.py
+++ homeworks-small/shw-01-mlp/modules/criterions.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.special as sps
 from .base import Criterion
 from .activations import LogSoftmax
 
@@ -15,8 +14,8 @@ class MSELoss(Criterion):
         :param target:  array of size (batch_size, *)
         :return: loss value
         """
-        assert input.shape == target.shape, f"input and target shapes not matching: {input.shape = }; {target.shape = }"
-        return ((input - target) ** 2).mean()
+        assert input.shape == target.shape, 'input and target shapes not matching'
+        return np.mean(np.square(input - target))
 
     def compute_grad_input(self, input: np.array, target: np.array) -> np.array:
         """
@@ -24,9 +23,9 @@ class MSELoss(Criterion):
         :param target:  array of size (batch_size, *)
         :return: array of size (batch_size, *)
         """
-        assert input.shape == target.shape, f"input and target shapes not matching {input.shape = }; {target.shape = }"
-        n = input.size
-        return 2 * (input - target) / n
+        assert input.shape == target.shape, 'input and target shapes not matching'
+        b, n = input.shape
+        return (2 * input - 2 * target) / (b * n)
 
 
 class CrossEntropyLoss(Criterion):
@@ -44,7 +43,16 @@ class CrossEntropyLoss(Criterion):
         :param target: labels array of size (batch_size, )
         :return: loss value
         """
-        return -sps.log_softmax(input, axis=1)[np.arange(len(target)), target].mean()
+        batch_size, num_classes = input.shape
+        log_p = self.log_softmax.compute_output(input)
+
+        reshaped_target = target.reshape(
+            (batch_size, 1)).repeat(num_classes, 1)
+        reshaped_classes = np.arange(num_classes).reshape(
+            (1, num_classes)).repeat(batch_size, 0)
+        indicators = np.array(reshaped_target == reshaped_classes, dtype=int)
+
+        return -np.sum(log_p * indicators) / batch_size
 
     def compute_grad_input(self, input: np.array, target: np.array) -> np.array:
         """
@@ -52,7 +60,12 @@ class CrossEntropyLoss(Criterion):
         :param target: labels array of size (batch_size, )
         :return: array of size (batch_size, num_classes)
         """
-        B = input.shape[0]
-        ones = np.zeros_like(input)
-        ones[np.arange(len(target)), target] = 1
-        return (sps.softmax(input, axis=1) - ones) / B
+        batch_size, num_classes = input.shape
+
+        reshaped_target = target.reshape(
+            (batch_size, 1)).repeat(num_classes, 1)
+        reshaped_classes = np.arange(num_classes).reshape(
+            (1, num_classes)).repeat(batch_size, 0)
+        indicators = np.array(reshaped_target == reshaped_classes, dtype=int)
+
+        return -self.log_softmax.compute_grad_input(input, indicators) / batch_size
